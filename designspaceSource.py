07f2a6d23772977988909c9edd554b3dd7dec301
thomas bouillet <thomasbouillet@MacBook-Pro-de-thomas-3.local>
2019-12-07 19:31:14 +0100

    source has been added, and output too

---
+++ designspaceSource.py
@@ -0,0 +1,44 @@
+from fontTools.designspaceLib import *
+
+document = DesignSpaceDocument()
+
+weightAxis = AxisDescriptor()
+weightAxis.maximum = 170
+weightAxis.minimum = 50
+weightAxis.default = 50
+weightAxis.name = 'weight'
+weightAxis.tag = 'wght'
+weightAxis.labelNames[u'fr'] = u'poids'
+weightAxis.labelNames[u'en'] = u'weight'
+weightAxis.labelNames[u'it'] = u'peso'
+document.addAxis(weightAxis)
+
+styles = {
+    'light': [50, 'path'],
+    'black': [170, 'path'],
+    'medium': [120, 'desktop/file.ufo']
+    }
+
+
+for i, (style, (pos, path)) in enumerate(styles.items()):
+    source = SourceDescriptor()
+    source.path = path
+    source.name = style
+    source.familyName = "Jatoru"
+    source.styleName = style
+    source.location = dict(weight=pos)
+    if i == 0:
+        source.copyLib = True
+        source.copyInfo = True
+        source.copyGroups = True
+        source.copyFeatures = True
+    document.addSource(source)
+
+rule = RuleDescriptor()
+rule.name = 'substitution.g'
+rule.conditionSets.append([dict(name='weight', minimum=100, maximum=170)])
+rule.subs.append(('g', 'g.alt'))
+document.addRule(rule)
+
+
+document.write('Jatoru.designspace')
